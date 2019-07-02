2c3e072b238f6a07249725c62e706b7b3a72815b
bgginley <bgginley@gmail.com>
2019-07-02 10:36:32 -0400

    Add files via upload

--- write_xml_annotations.m
+++ write_xml_annotations.m
@@ -1,12 +1,18 @@
 close all
 clear all
 
-xml_path='C:\Users\bgginley\Desktop\Microvascular_invasion\\Good Quality\S15-70548.1.A.4 - 2016-12-05 18.40.22.xml';
-xml_out='C:\Users\bgginley\Desktop\test.xml';
-annot_path='C:\Users\bgginley\Desktop\Microvascular_invasion\Good Quality\S15-70548.1.A.4 - 2016-12-05 18.40.22_16170_23576.png';
+
+ID='S16-5205 - 2016-12-12 16.53.30';
+
+xml_path=['C:\Users\bgginley\Desktop\Microvascular_invasion\\Good Quality\',ID,'.xml'];
+xml_out=xml_path;
+
+label_offsets=[11190,12751];
+annot_path=['C:\Users\bgginley\Desktop\Microvascular_invasion\Good Quality\',ID,'_',num2str(label_offsets(1)),'_',num2str(label_offsets(2)),'.png'];
+
 
 mod = py.importlib.import_module('mask_to_xml');
 xml_color = [65280, 65535, 255, 16711680, 33023];
-label_offsets=[16170,23576];
+
 
 py.mask_to_xml.mask_to_xml(xml_path,annot_path,label_offsets,xml_out, 1,1,1)
